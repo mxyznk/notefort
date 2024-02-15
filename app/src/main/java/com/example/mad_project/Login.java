23e0e5f027179e44398ce6eb4a761f1c017c31ef
mxyznk <maumayank99@gmail.com>
2024-02-15 09:01:17 +0530

    Added Google, FaceBook, GitHub Button. Intent to go from Welcome screen to Login Screen

--- app/src/main/java/com/example/mad_project/Login.java
+++ app/src/main/java/com/example/mad_project/Login.java
@@ -1,14 +1,52 @@
 package com.example.mad_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    EditText user_email, user_pw;
+    Button btn_login;
 
+    ImageView btn_google, btn_facebook, btn_github;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        user_email=findViewById(R.id.user_email);
+        user_pw=findViewById(R.id.user_pw);
+        btn_login=findViewById(R.id.btn_login);
+        btn_google=findViewById(R.id.btn_google);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email=String.valueOf(user_email.getText());
+                password=String.valueOf(user_pw.getText());
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Login.this, "Email cant be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }if(TextUtils.isEmpty(password)){
+                    Toast.makeText(Login.this, "Password cant be blank", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -2,25 +2,27 @@ package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView h1;
+
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=findViewById(R.id.continuebtn);
-        h1=findViewById(R.id.h1);
-        h1.setLetterSpacing(-4);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
